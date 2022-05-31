06a70a2dc45940f492f189fadbc1d852dafa323d
Daniel Hedrick <daniel.hedrick@razorvision.net>
2022-05-31 12:00:27 -0600

    Add an /about page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import { Link } from "gatsby"
+import * as React from "react"
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
+      <p>Howdy. I'm Daniel.</p>
+    </main>
+  )
+}
+
+export default AboutPage
