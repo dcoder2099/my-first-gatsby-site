9b303dac130ed090ab2c176b7b00faa78a48c19f
Daniel Hedrick <daniel.hedrick@razorvision.net>
2022-06-01 12:34:48 -0600

    Add a blog

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,8 +29,11 @@ const Layout = ({ pageTitle, children }) => {
           <li className={navLinkItem}>
             <Link to="/" className={navLinkText}>Home</Link>
           </li>
-          <li>
+          <li className={navLinkItem}>
             <Link to="/about" className={navLinkText}>About</Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>Blog</Link>
           </li>
         </ul>
       </nav>
